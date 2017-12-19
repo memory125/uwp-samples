a64e092ae9d698c38e96220dcc1bfa23984ea3db
De Meng (Thundersoft) <v-demen@microsoft.com>
2017-12-19 19:48:01 +0800

    Add the ImageShow sample

---
+++ ImageShow/ImageShow/MainPage.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace ImageShow
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    ///   
+    public sealed partial class MainPage : Page
+    {
+        private int count = 0;
+        DispatcherTimer _imageTimer = new DispatcherTimer();
+        public MainPage()
+        {
+            _imageTimer.Interval = new TimeSpan(0, 0, 10);
+            _imageTimer.Tick += Time_Tick;
+            this.InitializeComponent();
+            _imageTimer.Start();
+        }
+
+        private void Time_Tick(object sender, object e)
+        {
+            switch (count)
+            {
+                case 0:
+                    testImage.Source = new BitmapImage(new Uri("ms-appx:///Assets/1.jpg"));
+                    count++;                    
+                    break;
+
+                case 1:
+                    testImage.Source = new BitmapImage(new Uri("ms-appx:///Assets/2.jpg"));
+                    count++;
+                    break;
+
+                case 2:
+                    testImage.Source = new BitmapImage(new Uri("ms-appx:///Assets/3.jpg"));
+                    count++;
+                    break;
+
+                case 3:
+                    testImage.Source = new BitmapImage(new Uri("ms-appx:///Assets/4.jpg"));
+                    count++;
+                    break;
+
+                case 4:
+                    testImage.Source = new BitmapImage(new Uri("ms-appx:///Assets/5.jpg"));
+                    count++;
+                    break;
+
+                case 5:
+                    testImage.Source = new BitmapImage(new Uri("ms-appx:///Assets/6.jpg"));
+                    count++;
+                    break;
+
+                case 6:
+                    testImage.Source = new BitmapImage(new Uri("ms-appx:///Assets/7.jpg"));
+                    count++;
+                    break;
+
+                case 7:
+                    testImage.Source = new BitmapImage(new Uri("ms-appx:///Assets/8.jpg"));
+                    count++;
+                    break;
+
+                case 8:
+                    testImage.Source = new BitmapImage(new Uri("ms-appx:///Assets/9.jpg"));
+                    count++;
+                    break;
+
+                case 9:
+                    testImage.Source = new BitmapImage(new Uri("ms-appx:///Assets/10.jpg"));
+                    count++;
+                    break;
+
+                case 10:
+                    testImage.Source = new BitmapImage(new Uri("ms-appx:///Assets/11.jpg"));
+                    count++;
+                    break;
+
+                case 11:
+                    testImage.Source = new BitmapImage(new Uri("ms-appx:///Assets/azure.png"));
+                    count = 0;
+                    break;
+            }            
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # uwp-samples
-This repo is for uwp samples
+> This repo is focusing on UWP samples.
+
+## ImageShow
+> This sample is used to loading picture via Image control, and it can change the image automatically.
+>* Install [Visual Studio 2017](https://www.visualstudio.com/zh-hans/)
+>* Install the latest Windows SDK (16299).
+>* Open this project by Visual Studio 2017.
+>* F5 to run this sample.
